990214fa59b23d236f8cf1bec21ea4433079decc
Daniel Antal <antaldaniel@users.noreply.github.com>
2022-06-02 16:10:00 +0200

    EU SILC use case

---
+++ use_cases/EU_SILC.R
@@ -0,0 +1,6 @@
+## Please use this as a reference:
+## https://retroharmonize.dataobservatory.eu/articles/cap.html
+## You will need the latest, 0.2.5 version of retroharmonize installed from Github.
+
+
+
